4ff19b008a7958da39773981ee3a4df5750b4bcb
Denesh Anandathasan <denesh555@gmail.com>
2024-01-19 12:29:47 -0500

    Color Scheme Switcher

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
 // Write your DOM code here!
+
+//document.addEventListener("DOMContentLoaded", function () {
+
+const redLi =  document.querySelector('.red')
+redLi.addEventListener('click',() => {
+    document.body.style.background="red"
+})
+
+const whiteLi = document.querySelector('.white')
+whiteLi.addEventListener('click',() => { 
+    //document.body.classList.toggle="white"
+    document.body.style.background="white"
+})
+
+const blueLi = document.querySelector('.blue')
+blueLi.addEventListener('click', () => {
+    // if (document.body.classList.contains("blue")){
+    //     document.body.classList.remove("blue")
+    // }else {
+    // document.body.classList.add("blue")
+
+    //document.body.classList.toggle('blue')
+    document.body.style.background="blue"
+}
+)
+
+const yelloLi =  document.querySelector('.yellow')
+yelloLi.addEventListener('click',() => {
+    document.body.style.background="yellow"
+})
+
+//})
